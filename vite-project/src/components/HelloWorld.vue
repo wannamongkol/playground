d5cd23fe6998889d4805e8c16eacc1edf26d750c
Wannamongkol Sriwan <wannamongkol.sr@chococrm.com>
2024-09-04 11:15:19 +0700

    update helloworld component

--- vite-project/src/components/HelloWorld.vue
+++ vite-project/src/components/HelloWorld.vue
@@ -1,41 +1,44 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+const isDisabled = ref(true)
+const obj1 = reactive({
+  test : "reactive"
+})
+
+
+const switchBtn = () => {
+  isDisabled.value = !isDisabled.value; 
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
+  <h2>{{ obj1.test }}</h2>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <button type="button" @click="count++" :disabled="isDisabled">count is {{ count }}</button>
+
+    <button @click="switchBtn">switch</button>
+ 
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div class="container">
+
+  </div>
+
+
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+
+.container {
+  padding: 100px;
+  background-color: #f5f5f5;
+  border-radius: 10px;
 }
+
 </style>
